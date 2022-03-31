36ae2aa491d262986a8ec45338d88cbf480ab997
Merijn Trimbos <merijn.trimbos@gmail.com>
2022-03-31 23:15:44 +0200

    added level 3

--- BPW_2/Assets/Scripts/Player_Manager.cs
+++ BPW_2/Assets/Scripts/Player_Manager.cs
@@ -6,9 +6,12 @@ using UnityEngine.SceneManagement;
 public class Player_Manager : MonoBehaviour
 {
     public bool nearTable;
+    public bool nearOtherTable;
     public LayerMask Table;
+    public LayerMask OtherTable;
     public Camera cam1;
     public Camera cam2;
+    public Camera cam3;
 
     public static bool isPause;
 
@@ -21,6 +24,8 @@ public class Player_Manager : MonoBehaviour
     {
         cam1.enabled = true;
         cam2.enabled = false;
+        cam3.enabled = false;
+        
 
     }
 
@@ -28,6 +33,7 @@ public class Player_Manager : MonoBehaviour
     void Update()
     {
         nearTable = Physics.CheckSphere(transform.position, 2f, Table);
+        nearOtherTable = Physics.CheckSphere(transform.position, 2f, OtherTable);
 
         if (nearTable == true)
         {
@@ -36,6 +42,27 @@ public class Player_Manager : MonoBehaviour
                 cam1.enabled = !cam1.enabled;
                 cam2.enabled = !cam2.enabled;
                 if (cam2.enabled)
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    isPause = true;
+                }
+                else
+                {
+                    isPause = false;
+                    Cursor.lockState = CursorLockMode.Locked;
+                }
+            }
+
+        }
+
+        if (nearOtherTable == true)
+        {
+            Debug.Log("poep");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                cam1.enabled = !cam1.enabled;
+                cam3.enabled = !cam3.enabled;
+                if (cam3.enabled)
                 {
                     Cursor.lockState = CursorLockMode.None;
                     isPause = true;
